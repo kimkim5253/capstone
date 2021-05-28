5e0a64e50241b819286abf365095f897f01255bf
김기민 <Kimkimin@gimgimins-MacBook-Pro.local>
2021-05-28 14:58:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/capstone/CameraActivity.java
@@ -0,0 +1,79 @@
+package com.example.capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private Button captureImageBtn, detectImageBtn;
+    private ImageView imageView;
+    private TextView textView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        captureImageBtn = findViewById(R.id.capture_button);
+        detectImageBtn = findViewById(R.id.detected_button);
+        imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.text_display);
+
+        captureImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        detectImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                detectTextFromImage();
+            }
+        });
+    }
+
+
+
+    private void dispatchTakePictureIntent ()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult ( int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void detectTextFromImage()
+    {
+        //FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromMediaImage(mediaImage);
+
+    }
+}
